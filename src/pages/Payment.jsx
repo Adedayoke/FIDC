8c78d16867a76feb70873b1dd3f999e6158fe23c
Adedayoke <adedayoke2006@gmail.com>
2024-05-23 15:29:16 +0100

    payment initialization

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useContext } from "react";
 import "./styles.css";
+import { FaUserAlt } from "react-icons/fa";
+import { GiPadlock } from "react-icons/gi";
+import { AuthUserContext } from "../Context/AuthUserContext";
+import { useNavigate } from "react-router-dom";
 
 const Payment = () => {
+  const { setuserLoggedIn } = useContext(AuthUserContext);
+  const navigate = useNavigate();
+  const setPage = () => {
+    setuserLoggedIn(true);
+    navigate("/");
+  };
   return (
-    <div>Payment</div>
-  )
-}
+    <div className="payment">
+      {/* <div className="PaymentCont"> */}
+      <h1>Full time Undergraduate Student</h1>
+      <div className="paymentCont">
+        <h2>PAYMENT</h2>
+        <form action="">
+          <div className="inpt">
+          <label htmlFor="">CARDHOLDER'S NAME</label>
+          <div className="inputSection">
+            <div className="icon">
+              <FaUserAlt />
+            </div>
+            <input type="text" />
+          </div>
+          </div>
+          <br />
 
-export default Payment
+          <div className="inpt">
+          <label htmlFor="">CARD NUMBER</label>
+          <div className="inputSection">
+            <div className="icon">
+              <GiPadlock />
+            </div>
+            <input type="text" />
+          </div>
+          </div>
+          <br />
+          <div className="otherdet">
+            <div className="left">
+              <label htmlFor="">EXPIRY DATE</label>
+              <div className="inputSection">
+                <div className="icon">
+                  <GiPadlock />
+                </div>
+                <input type="text" placeholder="" />
+              </div>
+            </div>
+            <div className="right">
+              <label htmlFor="">CVV</label>
+              <div className="inputSection">
+                <div className="icon">
+                  <GiPadlock />
+                </div>
+                <input type="text" />
+              </div>
+            </div>
+          </div>
+          <br />
+        </form>
+        <button onClick={() => setPage()} className="pry_btn">
+          Submit
+        </button>
+      </div>
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default Payment;
